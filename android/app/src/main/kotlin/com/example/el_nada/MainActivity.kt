eca6af957437ab4dde0b3d3af31dbad4859072f1
ahmed ibrahim <ahmedibrahim5544332211@gmail.com>
2025-05-10 23:50:35 +0300

    Deploy Flutter Web

--- android/app/src/main/kotlin/com/example/el_nada/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.el_nada
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/core/routing/routes.dart
+++
@@ -1,7 +0,0 @@
-abstract class Routes {
-  static const String home = '/home';
-  static const String homeView = '/homeView';
-  static const String detailsView = '/detailsView';
-
-
-}
